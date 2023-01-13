7f96632347d882dea82253b3bca81ce9979fdcfc
TMuellerBE <122528075+TMuellerBE@users.noreply.github.com>
2023-01-13 11:37:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,15 +42,20 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error();
-#debuggin
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list where is_active=true")
-my_data_row = my_cur.fetchall()
+    
 streamlit.header("The active fruit load list contrains")
-streamlit.dataframe(my_data_row)
+#SN
+def get_fruit_load_list()
+  with my_cnx.cursor as my_cur:
+  my_cur.execute("SELECT * FROM fruit_load_list where is_active=true")    
+  return my_data_row = my_cur.fetchall()
+
+#add a button
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
 
 fruit_add_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', fruit_add_choice)
